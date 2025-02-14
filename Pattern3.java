1b2bb0e3da203a8c7a88d7db2ff08c5b031cf23a
Akshada0612 <155293231+Akshada0612@users.noreply.github.com>
2025-02-14 11:09:16 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,24 @@
+class Pattern3
+{
+	public static void main(String[] args) 
+	{
+		int n=10;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i;j++)
+			{
+				if(j<1)
+				{
+				System.out.print(" ");
+				}else
+				{
+					System.out.print("*");
+				}
+			
+			}
+			System.out.println();
+		}
+		
+	}
+}
+
